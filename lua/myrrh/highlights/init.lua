d62431a146465ae7f3bbdf75870813be8a0f2617
Akiyama Kohane <echo.aimless@gmail.com>
2025-10-19 14:27:21 +0800

    fix: pcall return value mistake usage

--- lua/myrrh/highlights/init.lua
+++ lua/myrrh/highlights/init.lua
@@ -22,10 +22,12 @@ function M.setup(theme_palette, config)
   local highlights = {}
 
   for _, name in ipairs(highlight_setting_list) do
-    local setting = pcall(require, 'myrrh.highlights.' .. name)
-    --- @diagnostic disable-next-line: undefined-field
-    for hl_name, spec in pairs(setting.setup(theme_palette, config)) do
-      highlights[hl_name] = spec
+    local ok, setting = pcall(require, 'myrrh.highlights.' .. name)
+    if ok then
+      --- @diagnostic disable-next-line: undefined-field, need-check-nil
+      for hl_name, spec in pairs(setting.setup(theme_palette, config)) do
+        highlights[hl_name] = spec
+      end
     end
   end
 
